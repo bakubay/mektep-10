dbcc77820b84e684e2d961467701430351667c3e
Gassyr <grasbaku@gmail.com>
2021-12-03 00:55:55 +0800

    feat: quiz area under the course content

--- src/components/CourseContentArea.vue
+++ src/components/CourseContentArea.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col p-2 border-green-400">
     <course-video />
     <div class="flex w-full h-full mt-5 justify-between text-xl">
       <button class="bg-green-200 p-3 w-12 hover:bg-green-300">
@@ -8,6 +8,9 @@
       <button class="bg-green-200 p-3 w-12 hover:bg-green-300">
         <span>→</span>
       </button>
+    </div>
+    <div class="flex justify-center h-52 border-2 mt-6 p-2 bg-green-100">
+        <h1 class="text-xl ">Quiz</h1>
     </div>
   </div>
 </template>
--- src/views/Course.vue
+++ src/views/Course.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="flex">
-      <sidebar />
-      <content-wrapper :title="$route.params.name">
-          <course-content-area />
-      </content-wrapper>
+    <sidebar :items="items" />
+    <content-wrapper :title="$route.params.name">
+      <course-content-area />
+    </content-wrapper>
   </div>
 </template>
 
 <script>
-import ContentWrapper from '../components/ContentWrapper.vue'
-import Sidebar from '../components/Sidebar.vue'
-import CourseContentArea from '../components/CourseContentArea.vue'
+import ContentWrapper from "../components/ContentWrapper.vue";
+import Sidebar from "../components/Sidebar.vue";
+import CourseContentArea from "../components/CourseContentArea.vue";
 export default {
-  components: { Sidebar , ContentWrapper, CourseContentArea},
-
-}
+  components: { Sidebar, ContentWrapper, CourseContentArea },
+  data() {
+    return {
+      items: [
+        {
+          courseName: "Math for beginners",
+          courseUrl: "google.com",
+        },
+        {
+          courseName: "Biology I",
+          courseUrl: "blahblahblah",
+        },
+        {
+          courseName: "Biology I",
+          courseUrl: "blahblahblah",
+        },
+        {
+          courseName: "Biology I",
+          courseUrl: "blahblahblah",
+        },
+        {
+          courseName: "Biology I",
+          courseUrl: "blahblahblah",
+        },
+        {
+          courseName: "Biology I",
+          courseUrl: "blahblahblah",
+        },
+        {
+          courseName: "Biology I",
+          courseUrl: "blahblahblah",
+        },
+        {
+          courseName: "Biology I",
+          courseUrl: "blahblahblah",
+        },
+        {
+          courseName: "Biology I",
+          courseUrl: "blahblahblah",
+        },
+      ],
+    };
+  },
+};
 </script>
