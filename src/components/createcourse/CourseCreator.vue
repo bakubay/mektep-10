28cf40eb90b78b81e57932c8a0459ae81353905e
Gassyr <grasbaku@gmail.com>
2021-12-06 23:03:03 +0800

    feat: Add scroll on add section, and course create form

--- src/components/createcourse/CourseCreator.vue
+++ src/components/createcourse/CourseCreator.vue
@@ -1,50 +1,57 @@
 <template>
   <div class="flex">
-    <div class="flex flex-col w-full min-h-full  py-2 my-4">
-      <div class="text-base">Here you can create courses by addingn sections and content.</div>
-      <course-creator-form v-for="(section, index) of courseSections" :key="section.id" :index="index" @section-saved="saveSession" />
+    <div class="flex flex-col w-full min-h-full py-2 my-4">
+      <div class="text-xl font-bold text-center mb-2">Enter course information</div>
+      <form action="" method="post">
+        <div class="flex flex-col border-2 shadow-md px-10 h-auto py-4">
+          <h1 class="text-gray-500"></h1>
+          <div class="flex items-center">
+            <div class="w-full flex py-3 items-center space-x-2">
+              <label class="font-semibold leading-none w-1/6">Course Name</label>
+              <input type="text" class="flex-1 leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 bg-gray-100 border rounded border-gray-200" />
+            </div>
+          </div>
+          <div class="flex items-center">
+            <div class="w-full flex py-3 items-center space-x-2">
+              <label class="font-semibold leading-none w-1/6">Image</label>
+              <input type="text" class="flex-1 leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 bg-gray-100 border rounded border-gray-200" />
+            </div>
+          </div>
+          <div class="flex items-center">
+            <div class="w-full flex space-x-2 pt-3">
+              <label class="font-semibold leading-none w-1/6">Category</label>
+              <input type="text" class="flex-1 leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 bg-gray-100 border rounded border-gray-200" />
+            </div>
+          </div>
+          <div class="flex items-center">
+            <div class="w-full flex space-x-2 pt-3">
+              <label class="font-semibold leading-none w-1/6">Description</label>
+              <textarea
+                type="text"
+                name="course-description"
+                rows="5"
+                cols="30"
+                class="flex-1 leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 bg-gray-100 border rounded border-gray-200"
+              ></textarea>
+            </div>
+          </div>
+
+          <div class="flex items-center w-full justify-end space-x-1">
+            <button @click="onSumbitSection" class="mt-4 leading-none py-4 px-10 bg-gray-300 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
+              Cancel
+            </button>
+            <button
+              @click="onSumbitSection"
+              class="mt-4 font-semibold leading-none text-white py-4 px-10 bg-green-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </form>
       <div class="flex mt-4 w-full justify-between">
-        <button @click="addSection" class="px-3 w-15 bg-green-200 hover:bg-blue-300">+</button>
-        <button @click="onSumbit" class="p-3 w-32 bg-green-500 hover:bg-green-300 rounded">Submit</button>
+        <button @click="onSumbit" class="p-3 w-32 bg-green-500 hover:bg-green-300 rounded">Create</button>
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import CourseCreatorForm from "../createcourse/CourseCreatorForm.vue";
-import { ref } from "vue";
-export default {
-  components: { CourseCreatorForm },
-  setup() {
-    let uuid = 1;
-    let courseSections = ref([
-      {
-        id: uuid,
-        title: "",
-        url: "",
-        description: "",
-      },
-    ]);
-    const addSection = () => {
-      uuid++;
-      courseSections.value.push({
-        id: uuid,
-        title: "",
-        url: "",
-        description: "",
-      });
-      console.log(uuid);
-    };
-    const saveSession = () => {};
-    return {
-      addSection,
-      saveSession,
-      courseSections,
-      uuid,
-    };
-  },
-};
-</script>
-
-<style></style>
--- src/views/CreateCourse.vue
+++ src/views/CreateCourse.vue
@@ -1,15 +1,17 @@
 <template>
   <content-wrapper :title="`Create Course`">
       <course-creator />
+      <course-lecture-creator />
   </content-wrapper>
 </template>
 
 <script>
 import ContentWrapper from '../components/ContentWrapper.vue'
 import CourseCreator from '../components/createcourse/CourseCreator.vue'
+import CourseLectureCreator from '../components/createcourse/CourseLectureCreator.vue'
 
 export default {
-  components: { ContentWrapper, CourseCreator },
+  components: { ContentWrapper, CourseLectureCreator, CourseCreator },
 
 }
 </script>
