9b3cc2a8e9ff45b1dac50ce9edfdc78b596aea84
Gassyr <grasbaku@gmail.com>
2022-01-08 16:15:35 +0800

    feat: Section navigation with arrow buttons

--- src/views/Section.vue
+++ src/views/Section.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="flex">
+  <div class="flex" v-if="currentSection">
     <sidebar :sections="sections" />
     <content-wrapper :title="currentSection.title">
       <course-content-area/>
-      <div class="flex w-full mt-5 justify-between text-xl">
-        <button v-if="currentSection.index != 1" class="bg-green-200 p-3 w-12 hover:bg-green-300">
+      <div class="w-full mt-5 text-xl">
+        <button v-if="this.currentSection.index !== 1" class="bg-green-200 p-3 w-12 hover:bg-green-300 float-left">
           <span @click="goToPreviousSection">←</span>
         </button>
-        <button v-if="currentSection.index != sections.length + 1" class="bg-green-200 p-3 w-12 hover:bg-green-300">
-          <span @click="goToNextSection">→</span>
+        <button @click="goToNextSection" v-if="this.currentSection.index !== sections.length" class="bg-green-200 p-3 w-12 hover:bg-green-300 float-right">
+          <span >→</span>
+        </button>
+        <button @click="true" v-if="this.currentSection.index == sections.length" class="flex bg-green-200 p-3 w-12 hover:bg-green-300 float-right">
+          Done
         </button>
       </div>
     </content-wrapper>
@@ -21,7 +24,6 @@ import Sidebar from "../components/Sidebar.vue";
 import CourseContentArea from "../components/CourseContentArea.vue";
 export default {
   components: {Sidebar, ContentWrapper, CourseContentArea },
-  
   computed:{
     sections(){
       return this.$store.state.currentSections;
@@ -32,10 +34,10 @@ export default {
   },
   methods: {
     goToNextSection() {
-      return this.sections.find((section) => section.index === this.$store.state.currentSection.index);
+      return this.$router.push({ name: 'Section', params: { sectionId: this.currentSection.index + 1 }})
     },
     goToPreviousSection(){
-      return 0;
+      return this.$router.push({ name: 'Section', params: { sectionId: this.currentSection.index - 1 }});
     }
   }
 };
