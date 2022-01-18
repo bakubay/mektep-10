7c28c355995e60e491d6748df025d823628ab635
gassyr.bakubay <gassyr.bakubay@lalamove.com>
2022-01-18 18:49:40 +0800

    feat: stepper for table of content

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,12 +5,20 @@
         <div class="px-3 py-2 font-semibold border-b-2 border-gray-200 border-transparent">Table of Content</div>
         <ul>
           <li v-for="(section, i) in sections" :key="i" :class="{ activeClass: sectionId == i + 1 }" class="hover:bg-gray-300 transition-colors duration-200">
-            <div class="flex items-center">
-              <div class="w-4 h-4 rounded-full border-2 shadow-sm border-gray-300 bg-white flex justify-center items-center">
-                <div class="w-2 h-2 rounded-full bg-green-300"></div>
+            <router-link :to="{ name: 'Section', params: { sectionId: section.index } }">
+              <div class="flex items-center">
+                <div class="grid grid-rows-3">
+                  <div class="border-r-2 h-4 mx-auto overflow-y-visible z-50" :class="i !== 0 ? 'opacity-100' : 'opacity-0'"></div>
+                  <div class="w-4 h-4 rounded-full border-2 shadow-sm border-gray-200 bg-white flex justify-center items-center">
+                    <div class="w-2 h-2 rounded-full bg-green-300"></div>
+                  </div>
+                  <div class="border-r-2 h-4 mx-auto overflow-y-visible z-50" :class="i !== sections.length - 1 ? 'opacity-100' : 'opacity-0'"></div>
+                </div>
+                <div class="px-3 py-2 relative block text-gray-900">
+                  {{ section.title }}
+                </div>
               </div>
-              <router-link class="px-3 py-2 relative block text-gray-900" :to="{ name: 'Section', params: { sectionId: section.index } }">{{ section.title }}</router-link>
-            </div>
+            </router-link>
           </li>
         </ul>
       </nav>
