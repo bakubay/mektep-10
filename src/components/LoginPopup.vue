11183c71569bc641210ea4448af47fcb36ce6980
Gassyr <grasbaku@gmail.com>
2022-01-19 08:09:08 +0800

    feat: refactoring with all recent changes

--- src/components/LoginPopup.vue
+++ src/components/LoginPopup.vue
@@ -4,8 +4,8 @@
     <div class="bg-white shadow-lg border-t-2 border-green-100 rounded-md flex flex-col px-6 py-4 justify-center items-center">
       <h2>Log in</h2>
       <form class="mt-2 flex flex-col gap-2 w-full px-4">
-        <input :disabled="isDisabled" placeholder="Email" v-model="email" type="text" :class="inputStyle" class="flex-1 leading-none text-gray-900 p-3 focus:outline-none focus:border-green-200 border rounded border-gray-200" />
-        <input :disabled="isDisabled" placeholder="Password" v-model="email" type="text" class="flex-1 leading-none text-gray-900 p-3 focus:outline-none focus:border-green-200 border rounded border-gray-200"/>
+        <input placeholder="Email" v-model="email" type="text" class="flex-1 leading-none text-gray-900 p-3 focus:outline-none focus:border-green-200 border rounded border-gray-200" />
+        <input placeholder="Password" v-model="password" type="text" class="flex-1 leading-none text-gray-900 p-3 focus:outline-none focus:border-green-200 border rounded border-gray-200"/>
       </form>
       <button class="mt-2 px-4 py-2 bg-green-200 hover:bg-green-100 rounded-md shadow-md text-gray-700">Sign in</button>
     </div>
@@ -15,7 +15,12 @@
 
 <script>
 export default {
-  
+  data(){
+    return{
+      email: "",
+      password: "",
+    }
+  }
 };
 </script>
 
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="sticky top-0 flex z-50 bg-white h-16 border-b-2 border-gray-100">
-    <div class="grid grid-cols-3 gap-4 items-center p-2 md-5 w-full ">
-      <button @click="toggleMobileMenu"><MenuAlt2Icon class="h-6 w-6 mr-4 text-gray-700 hover:text-gray-400 lg:hidden"/></button>
+    <div class="grid grid-cols-3 gap-4 items-center p-2 md-5 w-full">
+      <button @click="toggleMobileMenu"><MenuAlt2Icon class="h-6 w-6 mr-4 text-gray-700 hover:text-gray-400 lg:hidden" /></button>
       <div class="flex justify-center">
-        <router-link :to="{name: 'Home'}" class="font-semibold md:text-2xl text-gray-700">Mektep<span class="text-green-400">10</span></router-link>
+        <router-link :to="{ name: 'Home' }" class="font-semibold md:text-2xl text-gray-700">Mektep<span class="text-green-400">10</span></router-link>
       </div>
       <div class="flex gap-x-2 md:gap-x-5 items-center justify-end mr-2 md:mr-4">
-        <a class="text-gray-400 md:text-base text-sm hover:text-gray-700">Log in</a>
-        <router-link  class="text-gray-400 md:text-base text-sm hover:text-gray-700" to="/about">About</router-link >
-      </div>
+        <button @click="showLoginPopup" class="text-gray-400 md:text-base text-sm hover:text-gray-700">Log in</button>
+        <router-link class="text-gray-400 md:text-base text-sm hover:text-gray-700" to="/about">About</router-link>
       </div>
     </div>
-  <login-popup v-if="isLoginPopupActive" class="fixed right-0 mt-12 z-50"/>
+  </div>
+  <login-popup v-if="isLoginPopupActive" class="fixed right-0 mt-12 z-50" />
 </template>
 
 <script>
-import { MenuAlt2Icon } from '@heroicons/vue/outline';
-import LoginPopup from './LoginPopup.vue';
+import { MenuAlt2Icon } from "@heroicons/vue/outline";
+import LoginPopup from "./LoginPopup.vue";
 
 export default {
-  components: {MenuAlt2Icon, LoginPopup},
-  methods:{
-    toggleMobileMenu(){
-      console.log("Show Mobile menu")
-      this.$store.commit("toggleMobileMenu")
+  components: { MenuAlt2Icon, LoginPopup },
+  methods: {
+    toggleMobileMenu() {
+      console.log("Show Mobile menu");
+      this.$store.commit("toggleMobileMenu");
     },
-        showLoginPopup() {
+    showLoginPopup() {
       this.isLoginPopupActive = !this.isLoginPopupActive;
     },
   },
