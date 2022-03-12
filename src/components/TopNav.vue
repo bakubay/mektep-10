376934be13cb85dbc1f1ab647b92bb19419ee5c2
Gasssyr <grasbaku@gmail.com>
2022-03-13 01:45:28 +0800

    feat: login and logout functionalitie

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -14,15 +14,15 @@
         <router-link :to="{ name: 'Home' }" class="font-semibold md:text-2xl text-gray-700">Mektep<span class="text-green-400">10</span></router-link>
       </div>
       <div class="flex gap-x-2 md:gap-x-5 items-center justify-end mr-2 md:mr-4">
-        <button v-if="!this.$store.state.user" @click.stop="toggleLoginPopup" class="text-gray-400 md:text-base text-sm hover:text-gray-700">Log in</button>
+        <button v-if="!user" @click.stop="toggleLoginPopup" class="text-gray-400 md:text-base text-sm hover:text-gray-700">Log in</button>
         <router-link class="text-gray-400 md:text-base text-sm hover:text-gray-700" to="/about">About</router-link>
-        <div @click.stop="toggleAccountPopup" class="flex text-gray-400 md:text-base text-sm hover:text-gray-700 items-center">
+        <div v-if="user" @click.stop="toggleAccountPopup" class="flex text-gray-400 md:text-base text-sm hover:text-gray-700 items-center">
           <user-circle-icon class="h-6 w-6 mt-0.5 transform transition-transform"/>
         </div>
       </div>
     </div>
   </div>
-  <div v-if="isDropdownActive" v-click-outside="disableDropdown" class="absolute z-50 top-16 w-screen md:ml-4 px-6 py-4 mt-1 md:max-w-sm shadow-lg bg-white border-t-2 border-green-100">
+  <div v-show="isDropdownActive" v-click-outside="disableDropdown" class="absolute z-50 top-16 w-screen md:ml-4 px-6 py-4 mt-1 md:max-w-sm shadow-lg bg-white border-t-2 border-green-100">
     <div class="flex flex-col text-md md:text-base leading-6">
       <ul>
         <li>Arts</li>
@@ -33,8 +33,8 @@
       </ul>
     </div>
   </div>
-  <login-popup v-if="isLoginPopupActive" v-click-outside="disableLoginPopup" @close-login-popup="disableLoginPopup" class="fixed right-0 mt-12 z-50" />
-  <account-popup v-if="isAccountPopupActive" v-click-outside="disableAccountPopup" @close-account-popup="disableAccountPopup" class="fixed right-0 mt-12 z-50" />
+  <login-popup v-show="isLoginPopupActive" v-click-outside="disableLoginPopup" @close-login-popup="disableLoginPopup" class="fixed right-0 mt-12 z-50" />
+  <account-popup v-show="isAccountPopupActive" v-click-outside="disableAccountPopup" @close-account-popup="disableAccountPopup" class="fixed right-0 mt-12 z-50" />
 </template>
 
 <script>
@@ -70,7 +70,10 @@ export default {
   computed: {
     userName(){
       return this.$store.state.profileFirstName;
-      }
+      },
+    user(){
+      return this.$store.state.user;
+    }
   },
   data() {
     return {
